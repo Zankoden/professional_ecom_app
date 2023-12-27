9e1312bf89caf94cf436cf1a8124f9b8957a5f8a
Zankodits <zankodits@gmail.com>
2023-12-27 21:52:19 +0545

    basic task comment in main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:professional_ecom_app/utils/theme/theme.dart';
 
 void main() {
+  // Todo: Add Widgets Binding
+
+  // Todo: Init Local Storage
+
+  // Todo: Await Native Splash
+
+  // Todo: Initialize Firebase
+
+  // Todo: Initialize Authentication
+
   runApp(const MyApp());
 }
 
